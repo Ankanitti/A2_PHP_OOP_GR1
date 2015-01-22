5a8786e2ac0ff380c0547feb7703fe65d69d5791
Guillaume BEUZELIN <william12.5@hotmail.com>
2015-01-22 22:08:47 +0100

    added

---
+++ revive.php
@@ -0,0 +1,24 @@
+<?php
+
+require __DIR__.'/_header.php';
+
+$em = require __DIR__.'/bootstrap.php';
+
+$pokemonRepository = $em->getRepository('Ankanitti\PokemonBattle\PokemonModel');
+
+$isConnected = false;
+
+if(isset($_SESSION['connected']) && $_SESSION['connected'] = true){
+    $isConnected = true;
+    $criteria = array('trainerId' => $_SESSION['id']);
+    $userPokemon = $pokemonRepository->findOneBy($criteria);
+    $userPokemon->setHp(100);
+
+    $em->flush();
+
+    echo $twig->render('revive.html.twig',[
+        'isConnected' => $isConnected,
+    ]);
+}else{
+    header('Location: index.php');
+}
